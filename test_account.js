b91c03e2aae5a4738abff43db3d3960ec939f6c5
afik shani <afikshani@afiks-MacBook-Pro.local>
2018-12-30 22:41:59 +0200

    few changes in test_account module

--- test_account.js
+++ test_account.js
@@ -6,7 +6,11 @@
  * 
  * 
  * Read and Write permission within the token given to test account with user-name "afik21023201"
- * in order to change this account, enter the relevant credentials
+ * in order to change this account, do the following:
+ * 1) Enter to Twitter.com and create a new test account.
+ * 2) Apply for being a developer with Twitter.
+ * 3) Once you get approved, create a new app.
+ * 4) After doing that, type in the access token in the relevant credentails fields.
  */
 
 module.exports = {
